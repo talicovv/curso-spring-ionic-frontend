ae8caba166065ce6898c315764495605ce33ae3d
talicovv <soparacastro80@gmail.com>
2018-09-02 20:23:01 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 
 @Injectable()
@@ -33,6 +34,9 @@ export class ErrorInterceptor implements HttpInterceptor {
 				case 403:
 				this.handle403();
 				break;
+				case 422:
+				this.handle422(errorObj);
+				break;
 
 				default:
 				this.handleDefaultError(errorObj);
@@ -57,6 +61,22 @@ export class ErrorInterceptor implements HttpInterceptor {
 		this.storage.setLocalUser(null);
 	}
 
+	handle422(errorObj){
+		let alert = this.alertCtrl.create(
+			{
+				title: 'Inconsistência na validação',
+				message: this.listErrors(errorObj.errors),
+				enableBackdropDismiss: false,
+				buttons: [
+					{
+						text:'ok'
+					}
+				]
+			});
+			alert.present();	
+		
+	}
+
 	handleDefaultError(errorObj){
 		const alert = this.alertCtrl.create(
 			{
@@ -67,6 +87,16 @@ export class ErrorInterceptor implements HttpInterceptor {
 			}			
 		);
 		alert.present();
+	}
+
+	private listErrors(messages: FieldMessage[]):string{
+		let s : string = '';
+		for (var i=0; i< messages.length; i++ ){
+			s = s + '<p><strong> '+messages[i].fieldName + '</strong>: '+ messages[i].message + '</p>';
+
+		}
+		return s;
+
 	}
 	
 
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EstadoDTO } from '../../models/estado.dto';
 import { CidadeDTO } from '../../models/cidade.dto';
 import { CidadeService } from '../../services/domain/cidade.service';
 import { EstadoService } from '../../services/domain/estado.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -22,7 +23,9 @@ export class SignupPage {
     public navParams: NavParams,
     public formBuilder: FormBuilder,
     public cidadeService : CidadeService,
-    public estadoService : EstadoService
+    public estadoService : EstadoService,
+    public clienteService : ClienteService,
+    public alertCrtl : AlertController
     ) {
 
     this.formGroup = this.formBuilder.group({
@@ -65,6 +68,29 @@ export class SignupPage {
   }
 
   signupUser() {
-    console.log("enviou o form");
+    this.clienteService.insert(this.formGroup.value)
+    .subscribe(response => {
+      this.showInsertOk();
+    },
+    error => {});
   }
+
+  showInsertOk(){
+    let alert = this.alertCrtl.create(
+      {
+        title: 'Sucesso!',
+        message: 'Cadastro efetuado com sucesso!',
+        enableBackdropDismiss: false,
+        buttons:[{
+          text:'ok',
+          handler: () => {
+            this.navCtrl.pop();
+          }
+        }]
+
+      }
+    );
+    alert.present();
+  }
+
 }

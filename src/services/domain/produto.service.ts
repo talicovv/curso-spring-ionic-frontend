a645303557af6618ec1f2a1a34370c2d0a147db2
talicovv <soparacastro80@gmail.com>
2018-09-13 22:07:36 -0300

    carregando dados do produto

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
@@ -9,6 +10,10 @@ export class ProdutoService {
     constructor(public http: HttpClient) {
     }
 
+    findById(produto_id: string){
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
+    }
+
     findByCategoria(categoria_id : string)  {
     	
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
@@ -18,4 +23,10 @@ export class ProdutoService {
         let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
         return this.http.get(url, {responseType: 'blob'});
     }
+    
+    getImageFromBucket(id: string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}.jpg`
+        return this.http.get(url, {responseType: 'blob'});
+    }
+
 }

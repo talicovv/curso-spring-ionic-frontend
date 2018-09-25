35d5f6d50aefe662499dc17b0d6d9df6b29f1fdd
talicovv <soparacastro80@gmail.com>
2018-09-25 20:05:15 -0300

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -13,6 +13,11 @@ export class ClienteService{
         
     }
 
+    findById(id:string){
+
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+    }
+
     findByEmail(email:string){
 
         return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);

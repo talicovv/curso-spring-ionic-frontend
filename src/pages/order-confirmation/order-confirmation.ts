91d3c4ac49923e02b9524d6ccf94cccec1a5e5c4
talicovv <soparacastro80@gmail.com>
2018-09-25 21:03:03 -0300

    salvando pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -1,11 +1,12 @@
 import { ClienteService } from './../../services/domain/cliente.service';
 import { CartService } from './../../services/domain/cart.service';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { PedidoDTO } from '../../models/pedido.dto';
 import { CartItem } from '../../models/cart-Item';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 
 
@@ -25,7 +26,9 @@ export class OrderConfirmationPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService: PedidoService,
+    public alertCrtl : AlertController) {
 
       this.pedido = this.navParams.get('pedido');
   }
@@ -47,6 +50,46 @@ export class OrderConfirmationPage {
 
   total(){
     return this.cartService.total();
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
+  }
+
+  showInsertOk(){
+    let alert = this.alertCrtl.create(
+      {
+        title: 'Sucesso!',
+        message: 'Pedido efetuado com sucesso!',
+        enableBackdropDismiss: false,
+        buttons:[{
+          text:'ok',
+          handler: () => {
+            this.navCtrl.setRoot('CategoriasPage');
+          }
+        }]
+
+      }
+    );
+    alert.present();
+    
+  }
+
+  checkout(){
+    
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+          this.cartService.createOrClearCart();
+          console.log(response.headers.get('location'));
+          this.showInsertOk();          
+        },
+        error =>{
+          if (error.status == 403){
+            this.navCtrl.setRoot('HomePage');
+          }
+        }
+
+      );
   }
 
 

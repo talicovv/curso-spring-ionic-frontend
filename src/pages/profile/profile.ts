faca729729e42c7427dfda39e5b25230547c642d
talicovv <soparacastro80@gmail.com>
2018-10-14 13:56:04 -0300

    bugfix camera false

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -71,6 +71,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+     this.cameraOn = false;
     });
   }
 
@@ -89,6 +90,7 @@ export class ProfilePage {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
+     this.cameraOn = false;
     });
   }
 
